01a7e6b96d1de5444c9c6dbe97d0fb05875d6c49
zinja-coder <jafarpathan9090@gmail.com>
2025-10-31 21:56:26 +0530

    fix: fixed the issue 42 - banner unicode character encoding error by catching UnicodeEncodeError and printing normal message, 2 updated banner to v3.3.5

--- src/banner.py
+++ src/banner.py
@@ -15,6 +15,6 @@ def jadx_mcp_server_banner() -> str:
             
             Author         -> Jafar Pathan (zinja-coder@github)
             For Issues     -> https://github.com/zinja-coder/jadx-mcp-server
-            Server Version -> v3.3.3
+            Server Version -> v3.3.5
             
           """
